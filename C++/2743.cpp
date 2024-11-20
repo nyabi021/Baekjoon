f9a795a47b02a026bf2f3c4a2bee5999b075a395
Nyabi🥨🍚 <121873539+nyabi021@users.noreply.github.com>
2024-11-20 15:06:17 +0900

    Add files via upload

---
+++ C++/2743.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char str[101] = {0,};
+    cin >> str;
+    int a = strlen(str);
+    cout << a << endl;
+
+    return 0;
+}
